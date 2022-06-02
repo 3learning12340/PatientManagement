9f42ba830e170cad55bc3169f9a021f112a14f30
3learning12340 <perfect_insist@163.com>
2022-06-02 20:50:40 +0800

    idea second git Commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//    second
     public static void main(String[] args) {
+
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("second");
     }
 
 
