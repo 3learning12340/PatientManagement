99e9035dd0ec4c3a383ef2dfd84b38eb7c2de993
3learning12340 <perfect_insist@163.com>
2022-06-28 16:07:36 +0800

    idea ssh second Commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,7 @@ public class DemoApplication {
         System.out.println("second");
         System.out.println("...");
         System.out.println("pull test");
+        System.out.println("push test2");
     }
 
 
